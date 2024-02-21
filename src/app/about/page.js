c98fdfbdd9f4e616e463694c161c1757d8c105eb
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-21 08:38:54 -0800

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,83 +3,83 @@ import React from "react";
 const page = () => {
   return (
     <div class="-[90vw] md:w-[80vw] px-6 mx-auto">
-      <h1 className=" my-4 font-bold text-2xl">InstaSaver - Online Instagram Downloader</h1>
+      <h1 className=" my-4 font-bold text-2xl">YouTube Video Downloader - Online Youtube Downloader</h1>
       <p className="text-lg">
         <i>
-          How to download Instagram photos and videos? How to download videos
-          from Instagram privately? These are the questions that many Instagram
+          How to download Youtube photos and videos? How to download videos
+          from Youtube privately? These are the questions that many Youtube
           users are interested in. In this article I will show you how to
-          download photos and videos from Instagram using the online InstaSaver
+          download photos and videos from Youtube using the online YouTube Video Downloader
           tool.
         </i>
       </p>
       <p className="text-lg">
-        InstaSaver is an Instagram downloader, allow to download content
-        (videos, photos, story, Insta reels, IGTV) from Instagram in best
-        quality. Download Instagram photos and videos on web browser, support
+        YouTube Video Downloader is an Youtube downloader, allow to download content
+        (videos, photos, story, Insta reels, IGTV) from Youtube in best
+        quality. Download Youtube photos and videos on web browser, support
         any device PC, tablet, iPhone, Android.
       </p>
       <p className="text-lg">
-        In addition, InstaSaver also allows download private photos and videos
-        from Instagram. Just paste the Instagram link into the InstaSaver input box
+        In addition, YouTube Video Downloader also allows download private photos and videos
+        from Youtube. Just paste the Youtube link into the YouTube Video Downloader input box
         and you can download any Insta content without install software.
       </p>
       <p>
-        <img src="/InstaSaver-min.jpg" width="800" height="399" alt="InstaSaver" />
+        <img src="/YouTube Video Downloader-min.jpg" width="800" height="399" alt="YouTube Video Downloader" />
       </p>
       <p className="text-center font-semibold text-xl">
-        <em>Best Instagram Downloader 2024</em>
+        <em>Best Youtube Downloader 2024</em>
       </p>
-      <h2 className="text-xl font-semibold">Features of InstaSaver</h2>
+      <h2 className="text-xl font-semibold">Features of YouTube Video Downloader</h2>
       <p className="text-lg " >
-        - Support download Instagram videos in high quality: Full HD, 1080p, 2K,
+        - Support download Youtube videos in high quality: Full HD, 1080p, 2K,
         4K, 8K with sound.
       </p>
       <p className="text-lg ">
-        - Support download Instagram photos from posts easily, this feature
+        - Support download Youtube photos from posts easily, this feature
         allows you to choose the quality and size of images before saving
-        Instagram photos to the device.
+        Youtube photos to the device.
       </p>
       <p className="text-lg ">
-        - This feature allows download Instagram Reels videos in HD quality with
-        sound. Support download Instagram Reels on PC, iPhone, Android.
+        - This feature allows download Youtube Reels videos in HD quality with
+        sound. Support download Youtube Reels on PC, iPhone, Android.
       </p>
       <p className="text-lg ">
-        - Download Instagram Story or Highlights in high quality, you can
-        download Instagram Stories by username or link in 1080p.
+        - Download Youtube Story or Highlights in high quality, you can
+        download Youtube Stories by username or link in 1080p.
       </p>
       <p className="text-lg ">
-        - IGTV is Instagram long time video, this feature allows you to download
-        IGTV video from Instagram in high quality: 1080p, 2K, 4K, 8K without
+        - IGTV is Youtube long time video, this feature allows you to download
+        IGTV video from Youtube in high quality: 1080p, 2K, 4K, 8K without
         install software.
       </p>
       <p className="text-lg ">
-        - Allow to download Instagram post content from private account without
+        - Allow to download Youtube post content from private account without
         any restrictions, download Videos, Photos, Insta Reels, Story and IGTV
-        from private Instagram account for free.
+        from private Youtube account for free.
       </p>
       <p className="text-lg py-4">
         <i>
-          With InstaSaver you can download any Instagram content (Videos,
+          With YouTube Video Downloader you can download any Youtube content (Videos,
           Photos, Reels, Story, IGTV). We will continuously upgrade to bring you
           the best experience! Please share this tool with friends and family.
           Thank you!
         </i>
       </p>
-      <h2 className="text-xl font-semibold py-4">Steps to download Instagram photos and videos</h2>
+      <h2 className="text-xl font-semibold py-4">Steps to download Youtube photos and videos</h2>
       <p className="text-lg py-2">
-        <b>Step 1</b>: Open the Instagram app on your phone or go to the
-        Instagram.com website and log in to your account.
+        <b>Step 1</b>: Open the Youtube app on your phone or go to the
+        Youtube.com website and log in to your account.
       </p>
       <p className="text-lg py-2">
-        <b>Step 2</b>: Copy Url Instagram Photos &amp; Videos
+        <b>Step 2</b>: Copy Url Youtube Photos &amp; Videos
       </p>
       <p className="text-lg py-2">
         <b>Step 3</b>: Go to the website{" "}
         <a target="_blank" href="https://insta-saver.vercel.app/">
-          <b>InstaSaver</b>
+          <b>YouTube Video Downloader</b>
         </a>
-        , paste the Instagram link you just copied into input box and press the{" "}
+        , paste the Youtube link you just copied into input box and press the{" "}
         <strong>Download</strong> button.
       </p>
       <p className="text-lg py-2">
@@ -93,7 +93,7 @@ const page = () => {
       </p>
       <p className="text-lg py-2">
         During use, if you encounter an error, please contact us for support:{" "}
-        <a href="mailto:contact.instasaver@gmail.com">instasaver@gmail.com</a>
+        <a href="mailto:contact.YouTube Video Downloader@gmail.com">YouTube Video Downloader@gmail.com</a>
       </p>
     </div>
   );
