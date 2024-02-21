decc329be96571044a9fcfb0ea13253c9b794754
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-21 07:47:06 -0800

    Update page.js

--- src/app/how-to-download-instagram-video/page.js
+++ src/app/how-to-download-instagram-video/page.js
@@ -4,22 +4,22 @@ const page = () => {
   return (
     <>
       <div class="-[90vw] md:w-[80vw] mx-auto my-5">
-        <h1 class=" my-4 font-bold text-2xl">How to download videos & photos from Instagram online 2024</h1>
+        <h1 class=" my-4 font-bold text-2xl">How to download videos & Shorts from Youtube online 2024</h1>
         <p className="text-lg">
           <i>
-            How to download photos and videos from Instagram? This is a question
-            that many Instagram users are interested in, in this article I will
-            show you how to use InstaSaver to download photos and videos from
-            Instagram online on PC, iPhone, Android.
+            How to download Shorts and videos from Youtube? This is a question
+            that many Youtube users are interested in, in this article I will
+            show you how to use YouTube Video Downloader to download Shorts and videos from
+            Youtube online on PC, iPhone, Android.
           </i>
         </p>
         <p className="text-lg">
-          InstaSaver is an Instagram downloader, allow to download photos,
-          videos, Insta reels, story and IGTV from Instagram in the highest
-          quality. Just paste the Instagram link into the input box on InstaSaver
-          and you can download any Instagram post content for free.
+          YouTube Video Downloader is an Youtube downloader, allow to download Shorts,
+          videos, Insta reels, story and IGTV from Youtube in the highest
+          quality. Just paste the Youtube link into the input box on YouTube Video Downloader
+          and you can download any Youtube post content for free.
         </p>
-        <h2 className="text-xl font-semibold my-2 mt-5">Step 1: Copy Instagram link</h2>
+        <h2 className="text-xl font-semibold my-2 mt-5">Step 1: Copy Youtube link</h2>
         <p className="text-lg">
           Find the content you want to download and click on the icon above the
           post and then continue pressing the <strong>Copy Link</strong> option.
@@ -30,18 +30,18 @@ const page = () => {
             src="/how-to-web-step-1-min.jpg"
             width="800"
             height="527"
-            alt="InstaSaver"
+            alt="YouTube Video Downloader"
           />
         </p>
-        <h2 className="text-xl font-semibold my-2 mt-5">Step 2: Paste the copied Instagram link into InstaSaver</h2>
+        <h2 className="text-xl font-semibold my-2 mt-5">Step 2: Paste the copied Youtube link into YouTube Video Downloader</h2>
         <p className="text-lg">
           - Open a browser and go to the website:{" "}
           <a target="_blank" href="https://insta-saver.vercel.app/">
-            InstaSaver
+            YouTube Video Downloader
           </a>
         </p>
         <p className="text-lg">
-          - Paste the copied Instagram link into the input box and press the{" "}
+          - Paste the copied Youtube link into the input box and press the{" "}
           <strong>Download</strong> button.
         </p>
         <p className="text-lg">
@@ -50,10 +50,10 @@ const page = () => {
             src="/how-to-web-step-2-min.jpg"
             width="800"
             height="411"
-            alt="InstaSaver"
+            alt="YouTube Video Downloader"
           />
         </p>
-        <h2 className="text-xl font-semibold my-2 mt-5">Step 3: Download photos or videos to your device</h2>
+        <h2 className="text-xl font-semibold my-2 mt-5">Step 3: Download Shorts or videos to your device</h2>
         <p className="text-lg">
           - <strong>Photo</strong> and <strong>Video</strong> you want to
           download will appear, click <strong>Download Photo</strong> or{" "}
@@ -66,7 +66,7 @@ const page = () => {
             src="/how-to-web-step-3-min.jpg"
             width="800"
             height="418"
-            alt="InstaSaver"
+            alt="YouTube Video Downloader"
           />
         </p>
         <p className="text-lg">
@@ -77,15 +77,15 @@ const page = () => {
           download, use <b>Private downloader</b>:{" "}
           <a
             target="_blank"
-            href="https://insta-saver.vercel.app/instagram-private-downloader"
+            href="https://insta-saver.vercel.app/Youtube-private-downloader"
           >
-            https://insta-saver.vercel.app/instagram-private-downloader
+            https://insta-saver.vercel.app/Youtube-private-downloader
           </a>{" "}
           and follow the instructions to download your photo or video.
         </p>
         <p className="text-lg">
           During use, if you encounter an error, please contact us for support:{" "}
-          <a href="mailto:sp.InstaSaver@gmail.com">contact.instasaver@gmail.com</a>
+          <a href="mailto:sp.YouTube Video Downloader@gmail.com">contact.YouTube Video Downloader@gmail.com</a>
         </p>
       </div>
     </>
