943c40cad5f29f5c20c85772c3cdc927ddedb6b9
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-21 09:04:45 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-SWE4XHH1YZ"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-SWE4XHH1YZ');
