9e49bb98721dc5a4a35ed9ea2c6908c968e8bd72
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-21 08:44:07 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
     e.preventDefault();
     setdisabled("disabled");
     if (url) {
-      if (url.startsWith("https://you")) {
+      if (url.startsWith("https://www.you") || url.startsWith("https://you")) {
         const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}api/gettoken`, {
           method: "POST",
           headers: {
