233c86f71c5d35a3d578dcd97a1ddc161ee3119e
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-21 07:49:02 -0800

    Update page.js

--- src/app/disclaimer/page.js
+++ src/app/disclaimer/page.js
@@ -4,13 +4,13 @@ const page = () => {
   return (
     <div className='-[90vw] md:w-[80vw] px-6 mx-auto'>
 
-<h1 class=" my-4 font-bold text-2xl">Disclaimer for InstaSaver</h1>
+<h1 class=" my-4 font-bold text-2xl">Disclaimer for YouTube Video Downloader</h1>
 
-<p className="text-lg">If you require any more information or have any questions about our site's disclaimer, please feel free to contact us by email at instasaver@gmail.com. Our Disclaimer was generated with the help of the <a href="https://www.disclaimergenerator.net/">Free Disclaimer Generator</a>.</p>
+<p className="text-lg">If you require any more information or have any questions about our site's disclaimer, please feel free to contact us by email at youtubevideodownloader@gmail.com. Our Disclaimer was generated with the help of the <a href="https://www.disclaimergenerator.net/">Free Disclaimer Generator</a>.</p>
 
-<h2 className="text-xl font-semibold my-2 mt-5">Disclaimers for InstaSaver</h2>
+<h2 className="text-xl font-semibold my-2 mt-5">Disclaimers for YouTube Video Downloader</h2>
 
-<p className="text-lg">All the information on this website - www.instasaver.vercel.app - is published in good faith and for general information purpose only. InstaSaver does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (InstaSaver), is strictly at your own risk. InstaSaver will not be liable for any losses and/or damages in connection with the use of our website.</p>
+<p className="text-lg">All the information on this website - www.youtube-video-downloader.vercel.app - is published in good faith and for general information purpose only. YouTube Video Downloader does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (YouTube Video Downloader), is strictly at your own risk. YouTube Video Downloader will not be liable for any losses and/or damages in connection with the use of our website.</p>
 
 <p className="text-lg">From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone 'bad'.</p>
 
